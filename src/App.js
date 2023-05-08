cd191f8e84c8be15e376c3f1a0efca09600dd478
Keonwoo Kim <keonwookim@Keonwooui-MacBookPro.local>
2023-05-08 23:57:49 +1200

    welcome, first load re-designed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import MenuSelectors from "./components/MenuSelectors";
+import { useEffect, useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faK, faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+import Mainpage from "./page/Mainpage";
 
 import "./App.css";
 
 function App() {
+  const [isWelcome, setIsWelcome] = useState(true);
+  const [welcomeClass, setWelcomeClass] = useState("view__welcome appear");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWelcomeClass("view__welcome disappear");
+      setTimeout(() => {
+        setIsWelcome(false);
+      }, 2000);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <div className="main-page">
-        <MenuSelectors />
+      <div className="view">
+        <div className="view-top">
+          <div className="view-top__logo-line">
+            <FontAwesomeIcon className="view-top__logo" icon={faK} />
+          </div>
+        </div>
+        {isWelcome && (
+          <div className={welcomeClass}>
+            <h2>Welcome!</h2>
+            <FontAwesomeIcon
+              className="view__welcome__spinner"
+              icon={faSpinner}
+            />
+          </div>
+        )}
+        <Mainpage />
       </div>
     </div>
   );
---
+++ src/page/Mainpage.js
@@ -0,0 +1,10 @@
+import "./Mainpage.css";
+
+const Mainpage = () => {
+  return (
+    <div className="main">
+    </div>
+  );
+};
+
+export default Mainpage;
